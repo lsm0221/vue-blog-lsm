ced3da0b0323c0053064edf326d58666f994343b
lsm0221 <345088859@qq.com>
2019-08-24 10:42:32 +0800

    'publish'

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Header />
+    <!-- <keep-alive> -->
+    <router-view></router-view>
+    <!-- </keep-alive> -->
+  </div>
+</template>
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+<style>
+</style>
